398b2d4e5da827932d9b7e06563a0f609194631e
Prambhinfi8 <ifyoucanfindmeee@gmail.com>
2025-07-08 21:53:21 +0530

    pratice

--- test.py
+++ test.py
@@ -22,9 +22,6 @@ for char in range(1,bn_symbols+1):
     password_list +=random.choice(symbols)
 print(password_list)
 
-random.shuffle(password_list)
-print(password_list)
-
 password=""
 for char in password_list:
     password +=char
