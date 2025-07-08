8b26df8390bd47227ddd3e5f49716da37aadef8c
Prambhinfi8 <ifyoucanfindmeee@gmail.com>
2025-07-08 21:35:54 +0530

    FizzBizz Program

---
+++ Fizz_Buzz.py
@@ -0,0 +1,10 @@
+print("Welcome to The Fizz Buzz Program..")
+for number in range(1,101):
+    if number%3==0 and number%5==0:
+        print("FizzBuzz "+ str(number))
+    elif number%3==0:
+        print("Fizz "+ str(number))
+    elif number%5==0 :
+        print("Buzz "+ str(number))
+    else:
+        print(number)
