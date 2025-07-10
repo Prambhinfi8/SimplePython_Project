f7607994c1c52466a630ef750648b2210e0fdae7
Prambhinfi8 <ifyoucanfindmeee@gmail.com>
2025-07-10 15:06:28 +0530

    Phase_2 Practice

--- Hangman_game.py
+++ Hangman_game.py
@@ -4,11 +4,16 @@ import random
 
 word_list=["bhumil", "jay","jaydip"]
 chosen_word=random.choice(word_list)
+print(f"your guesse word is {chosen_word}")
+
+display=[]
+for letter in chosen_word:
+    display +="_"
+print(display)
 
 guess=input("Guess a letter:").lower()
 
 for letter in chosen_word:
     if letter==guess:
-        print("Right")
     else:
         print("wrong")
--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 import random
-word_list=["jay", "nisarg", "dev"]
-chosen_word=random.choice(word_list)
 
-guess=input("Guess a letter:").lower()
+word_list=["Bhumil", "jay", "jaydip"]
+
+chosen_word=random.choice(word_list)
+print(f"your chosen word is : {chosen_word}")
+display=[]
+for _ in range(len(chosen_word)):
+    display +="_"
+print(display)
+guess=input("guess the letter:").lower()
 
 for letter in chosen_word:
     if letter ==guess:
         print("right")
-    else:
+    else :
         print("wrong")
