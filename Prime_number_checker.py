57a286b95dbaa6c6198f835c48ff2a28de9b1671
Prambhinfi8 <ifyoucanfindmeee@gmail.com>
2025-07-11 16:45:06 +0530

    Prime_number_checker

--- Prime_number_checker.py
+++ Prime_number_checker.py
@@ -1,1 +1,18 @@
 print("Welcome to the Prime Number Checker")
+
+
+
+def prime_checker(number):
+    is_prime=True
+    for i in range(2,number):
+        if number%i==0:
+            is_prime=False
+
+    if is_prime==True:
+        print("It's Prime Number.")
+    else:
+        print("It's not prime Number.")
+
+
+n=int(input("Choose a Number:"))
+prime_checker(n)
